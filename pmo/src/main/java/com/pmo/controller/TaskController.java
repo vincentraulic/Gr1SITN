0334888b11ff76a7ce0fe34d438bafc848f4cd9d
Vincent Raulic <vincent.raulic@gmail.com>
2016-02-01 13:46:03 +0100

    Changed the taskcontroller

--- pmo/src/main/java/com/pmo/controller/TaskController.java
+++ pmo/src/main/java/com/pmo/controller/TaskController.java
@@ -3,21 +3,26 @@ package com.pmo.controller;
 import java.io.Serializable;
 
 import javax.ejb.EJB;
-import javax.enterprise.context.SessionScoped;
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
 
 import com.pmo.model.Task;
 import com.pmo.service.TaskService;
 
-@SessionScoped
+@RequestScoped
 @Named("taskController")
 public class TaskController implements Serializable{
-	
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 9148660482678453432L;
+
 	@EJB
 	private transient TaskService taskService;
-	
-	public int calculateProgression(Task task){
+
+	public int calculateProgression(Task task) {
 		return taskService.calculateProgression(task);
 	}
-
+	
 }
